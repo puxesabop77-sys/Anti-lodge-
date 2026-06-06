d16720a61a645a47c26570e5124da8cafe38e5a3
puxesabop77-sys <puxesabop77@gmail.com>
2026-06-06 08:57:45 +0530

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ftbbhyfrvriwjgztwrdi.supabase.co";
+const SUPABASE_KEY = "sb_publishable_WrmeRn9RNlfZwR3m8tO2Tw_wMEymeSB";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
